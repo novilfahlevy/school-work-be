d0117e65926584f8013f1fa4e531bda2a6e3ed38
mrizkimaulidan <mrizkimaulidan@hotmail.com>
2020-10-20 19:03:27 +0800

    balances seeder

---
+++ database/seeders/BalancesTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BalancesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('balances')->insert([
+            'balance' => 50000000,
+            'balanceable_type' => 'App\Loan',
+            'balanceable_id' => 1,
+            'changed_at' => date('Y-m-d H:i:s')
+        ]);
+    }
+}
