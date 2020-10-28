bd9ce0613346580391b87517e954469af4fa1235
mrizkimaulidan <mrizkimaulidan@hotmail.com>
2020-10-28 21:01:18 +0800

    added new migration

---
+++ database/migrations/2020_10_28_205733_create_loan_submissions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLoanSubmissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('loan_submissions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->integer('total_loan');
+            $table->date('start_date');
+            $table->tinyInteger('is_approve');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('loan_submissions');
+    }
+}
